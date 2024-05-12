779cf08ae99cb7772a6012c881d6563cb0fae7b1
zackyxd <zacharywalton501@gmail.com>
2024-05-11 21:07:49 -0700

    Testing the setup before working on real stuff

---
+++ src/events/ready.js
@@ -0,0 +1,15 @@
+const { Events, ActivityType } = require('discord.js');
+
+module.exports = {
+	name: Events.ClientReady,
+	once: true,
+	execute(client) {
+		console.log(`Ready! Logged in as ${client.user.tag}`);
+    client.user.setActivity({
+      name: "the server",
+      type: ActivityType.Watching
+    });
+	},
+	
+};
+
